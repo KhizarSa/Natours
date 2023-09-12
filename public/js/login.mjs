62f340cbf54f661ba389ed99ba7e4ec58bd60099
KhizarSa <khizarsaeed80@gmail.com>
2023-09-12 12:26:25 +0500

    Prepared app for deployment

--- public/js/login.mjs
+++ public/js/login.mjs
@@ -2,19 +2,15 @@
 import { showAlert } from './alerts.mjs';
 
 export const login = async (email, password) => {
-  console.log(email, password);
-
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
       },
     });
-
-    console.log(res);
 
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully');
@@ -31,11 +27,12 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
 
     if (res.data.status === 'success') location.reload(true);
   } catch (err) {
+    console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
